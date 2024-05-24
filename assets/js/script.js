d643e7abdc8c9beb410102441788f424e390ea43
bmich22 michel <barbmichel22@gmail.com>
2024-05-24 19:17:36 +0000

    Add functions to run quiz and check answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 // Wait for the DOM to finish loading before beginning the quiz
 // Listen for click on start button
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("start").addEventListener("click", function() {
-        alert("You clicked the start button!");
-      })
-    })      
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("start").addEventListener("click", function () {
+    alert("You clicked the start button!");
+    runQuiz();
+  })
+})
 
-// function runQuiz() {
-  const questionContent = [
+function runQuiz() {
+  let questionContent = [
     {
       num: "1",
       question: "this is question 1",
@@ -17,39 +18,58 @@ document.addEventListener("DOMContentLoaded", function() {
       choice3: "1C",
       choice4: "1D",
       correctAnswer: "1D",
-    }   
-  ]    
-    
-// }
+    }
+  ]
+  let correctAnswer = questionContent[0].correctAnswer;
 
-// function displayQuestion() {
+  document.getElementById("start-page").setAttribute("hidden", "hidden");
+  document.getElementById("quiz-page").removeAttribute("hidden");
+  document.getElementById("question").textContent = questionContent[0].question;
+  document.getElementById("choiceA").textContent = questionContent[0].choice1;
+  document.getElementById("choiceB").textContent = questionContent[0].choice2;
+  document.getElementById("choiceC").textContent = questionContent[0].choice3;
+  document.getElementById("choiceD").textContent = questionContent[0].choice4;
 
-// }
+  checkAnswer(correctAnswer);
 
-// function checkAnswer() {
+}
 
-// }
+function checkAnswer(correctAnswer) {
+  let choices = document.getElementById("choices-area");
+  choiceButtons = choices.getElementsByTagName("button");
 
-// function showResult() {
+  for (let button of choiceButtons) { 
 
-// }
+    button.addEventListener("click", function(){
+      if (this.textContent === correctAnswer) {
+        alert("correct");
+      } else {
+        alert("incorrect");
+      }
+    })
+  }
+}
 
-// function incrementCorrect() {
+function showResult() {
 
-// }
+}
 
-// function incrementWrong() {
+function incrementCorrect() {
 
-// }
+}
 
-// function showFinalScore() {
+function incrementWrong() {
 
-// }
+}
 
-// function nextQuestion() {
+function showFinalScore() {
 
-// }
+}
 
-// function startAgain() {
+function nextQuestion() {
 
-// }
+}
+
+function startAgain() {
+
+}
