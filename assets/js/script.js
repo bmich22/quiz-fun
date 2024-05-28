c85337d7cc05b83ced086a0cca444729ffabed19
bmich22 michel <barbmichel22@gmail.com>
2024-05-28 12:59:22 +0000

    Edit runQuiz function to only run if statement to run quiz through the quesion content array

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@ const choiceA =  document.getElementById("choiceA");
 const choiceB = document.getElementById("choiceB");
 const choiceC = document.getElementById("choiceC");
 const choiceD = document.getElementById("choiceD"); 
+const resultArea = document.getElementById("result-area");
+const resultText = document.getElementById("result-text");
+const correctNum = document.getElementById("correctNum");
+const wrongNum = document.getElementById("wrongNum");
 
 // Question Content
 const questionContent = [
@@ -14,12 +18,15 @@ const questionContent = [
     choice2: "1B",
     choice3: "1C",
     choice4: "1D",
-    correctAnswer: "1D",
+    correct: "1D",
   }
 ]
 
-// Set current question variable to zero
+// Set current question, correctScore and wrongScore variables to zero
 let currentQuestion = 0;
+let correctScore = 0;
+let wrongScore = 0;
+let result = "";
 
 // Wait for the DOM to finish loading before beginning the quiz
 // Listen for click on start button
@@ -32,30 +39,13 @@ document.addEventListener("DOMContentLoaded", function () {
 })
 
 function runQuiz() {
-  // let questionContent = [
-  //   {
-  //     num: "1",
-  //     question: "this is question 1",
-  //     choice1: "1A",
-  //     choice2: "1B",
-  //     choice3: "1C",
-  //     choice4: "1D",
-  //     correctAnswer: "1D",
-  //   }
-  // ]
-  // let quest = questionContent[0].questionText;
-  // let ch1 = questionContent[0].choice1;
-  // let ch2 = questionContent[0].choice2;
-  // let ch3 = questionContent[0].choice3;
-  // let ch4 = questionContent[0].choice4;
-  // let correctAns = questionContent[0].correctAnswer;
 
   if (currentQuestion < questionContent.length) {
     displayQuestion();
   } else {
     alert("Game Over!");
   }
-  // displayQuestion(quest, ch1, ch2, ch3, ch4, correctAns);
+
 }
 
 function displayQuestion() {
@@ -68,58 +58,60 @@ function displayQuestion() {
   choiceC.textContent = questionContent[currentQuestion].choice3;
   choiceD.textContent = questionContent[currentQuestion].choice4;
 
-  // checkAnswer();
 }
 
 function checkAnswer(value) {
-  alert(value);
-  // let choices = document.getElementById("choices-area");
-  // choiceButtons = choices.getElementsByTagName("button");
-
-  // for (let button of choiceButtons) {
-
-  //   button.addEventListener("click", function () {
-  //     let result = "";
-  //     alert(this.textContent);
-      
-  //     if (this.textContent === correctAns) {
-  //       alert("correct");
-  //       result = "Correct!"
-  //       // showResult(result); incrementCorrect();
-      
-  //     } else {
-  //       alert("incorrect");
-  //       result = "Incorrect!"
-  //       // showResult(result); incrementWrong();
-  //     }
-
-  //   })
+  // alert(value);
+  let inputAnswer = document.getElementById(value).textContent;
+  alert(inputAnswer);
+  alert(questionContent[currentQuestion].correct);
+  if (inputAnswer === questionContent[currentQuestion].correct) {
+    alert("CORRECT!");
+    incrementCorrect()
+    
+  } else {
+    alert("not correct!");
+    incrementWrong();
+  }
   }
 
-  function showResult(result) {
-    document.getElementById("result-area").removeAttribute("hidden");
-    document.getElementById("result-text").textContent = result;
-    document.getElementById("choiceA").disabled= true;
-    document.getElementById("choiceB").disabled= true;
-    document.getElementById("choiceC").disabled= true;
-    document.getElementById("choiceD").disabled= true;
+  function showResult() {
+    resultArea.removeAttribute("hidden");
+    resultText.textContent = result;
+    correctNum.textContent = correctScore;
+    wrongNum.textContent = wrongScore;
+    choiceA.disabled= true;
+    choiceB.disabled= true;
+    choiceC.disabled= true;
+    choiceD.disabled= true;
     
   }
 
   function incrementCorrect() {
-    let oldCorrectNum = parseInt(document.getElementById("correctNum").innerText);
-    document.getElementById("correctNum").innerText = ++oldCorrectNum;
+    correctScore += 1;
+    result = "that is indeed correct!";
+    alert(correctScore);
+    showResult();
   }
 
   function incrementWrong() {
-    let oldWrongNum = parseInt(document.getElementById("wrongNum").innerText);
-    document.getElementById("wrongNum").innerText = ++oldWrongNum;
+    wrongScore += 1;
+    alert(wrongScore);
+    result = "No, that is not correct!";
+    showResult();
   }
 
+  function nextQuestion() {
+    currentQuestion +=1;
+    runQuiz();
+  }
+
+
+  
+  // show final score and start again button
   function showFinalScore() {
  }
-  // function nextQuestion() {
- // }
-// function startAgain() {
-// }
+
+
+
 
